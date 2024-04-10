c8be97aeca82572e37eb4417c86e70c8df5ab6e4
SUHAS B M <91217762+i-suhas@users.noreply.github.com>
2024-04-10 13:27:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request
-import os
-import string
-import secrets
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 app = Flask(__name__)
 
